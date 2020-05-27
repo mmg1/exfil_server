8dc1a6da1c478e9a260ff7c110456bbb2cb0588c
multiplex3r <multiplex3r@gmail.com>
2020-05-27 10:28:06 +1000

    Exfil server

---
+++ README.md
@@ -0,0 +1,8 @@
+# Exfil server
+This is a quick and dirty flask app to upload files to over SSL.
+
+Install the requirements with `pip install -r requirements.txt`
+
+Run as per any other flask app with `python webserver.py`
+
+Modify the port etc from the webserver.py file if you want to bind to another port / interface
---
+++ webserver.py
@@ -0,0 +1,56 @@
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+
+# make a folder in the root of this app directory called "files" as the localtion for files to be uploaded to
+UPLOAD_FOLDER = 'files'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip', 'doc', 'docx', 'xls', 'xlsx', 'csv'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+#@app.route("/<filename>")
+#def uploaded_file(filename):
+#    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+#
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file'))
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+if __name__ == "__main__":
+    app.secret_key = "<a secure key>"
+    app.config['SESSION_TYPE'] = 'filesystem'
+    # you should gen ssl certs and put them below
+    app.run(host="127.0.0.1", port=31337, debug=True, ssl_context=("cert.pem", "key.pem"))
